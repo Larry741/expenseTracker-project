f720e1a41547edf262e4a377e9a1366b9da7c9c2
Larry741 <85595046+Larry741@users.noreply.github.com>
2022-03-08 11:17:57 +0100

    Finalizing updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import NewExpense from "./components/NewExpenses/NewExpense";
 import { useState } from "react";
 
 let dateString = "";
